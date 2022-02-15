18a12dd70f617a1dac8d10187c733e4c7a0922f0
Arthur Mabuti Pereira <arthurmabuti@hotmail.com>
2022-02-15 12:32:22 -0300

    Tratando erros com exceção personalizada

--- chess-console/Entities/Board/Board.cs
+++ chess-console/Entities/Board/Board.cs
@@ -14,15 +14,52 @@
         }
 
         // Allow other classes to access the position of pieces
-        public Piece piece(int line, int column)  
+        public Piece piece(int line, int column)
         {
             return Pieces[line, column];
         }
 
+        //Overcharge from Method piece() / Do the same thing but optimized
+        public Piece piece(Position pos)
+        {
+            return Pieces[pos.Line, pos.Column];
+        }
+
+        // This method is needed to validate if exist a piece in that space before moving another piece
+        public bool OccupiedSpace(Position pos)
+        {
+            PositionValidator(pos);
+            return piece(pos) != null;
+        }
+
         public void InsertPiece(Piece p, Position pos)
         {
+            if (OccupiedSpace(pos))
+            {
+                throw new BoardException("Position already occupied");
+            }
+
             Pieces[pos.Line, pos.Column] = p; // In position "pos.Line, pos.Column" insert Piece 'p'
             p.Position = pos; // Position of Piece 'p' is equal to "pos"
+
+        }
+
+        // This method exist just to be used in PositionValidar()
+        public bool ValidPosition(Position pos)
+        {
+            if (pos.Line < 0 || pos.Line >= Lines || pos.Column < 0 || pos.Column >= Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void PositionValidator(Position pos)
+        {
+            if (!ValidPosition(pos))
+            {
+                throw new BoardException("Invalid Position!");
+            }
         }
     }
 }
--- chess-console/Program.cs
+++ chess-console/Program.cs
@@ -3,13 +3,23 @@ using chess_console;
 using board;
 using chess;
 
-Board board = new Board(8, 8);
+try
+{
+    Board board = new Board(8, 8);
 
-board.InsertPiece(new Tower(Color.White, board), new Position(0, 0));
-board.InsertPiece(new Tower(Color.White, board), new Position(1,3));
-board.InsertPiece(new King(Color.White, board), new Position(2,4));
+    board.InsertPiece(new Tower(Color.White, board), new Position(0, 0));
+    board.InsertPiece(new Tower(Color.White, board), new Position(1, 3));
+    board.InsertPiece(new King(Color.White, board), new Position(2, 4));
 
-Screen.WriteBoard(board);
+    Screen.WriteBoard(board);
 
-Console.ReadLine();
-
+    Console.ReadLine();
+}
+catch (BoardException e)
+{
+    Console.WriteLine("Board error: " + e.Message);
+}
+catch(Exception e)
+{
+    Console.WriteLine("Unexpected error: " + e.Message);
+}
